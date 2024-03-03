f1ad3fef658a3be65fe268721de72cd19aa69037
heronlancellot <heronlancellot@id.uff.br>
2024-03-03 13:50:34 -0300

    feat: add TheHeader

---
+++ src/components/02-molecules/TheHeader.tsx
@@ -0,0 +1,12 @@
+export const TheHeader = () => {
+  return (
+    <div className="w-full h-auto top-0 fixed">
+      <div className="flex justify-center md:gap-8 md:text-2xl text-xl gap-4">
+        <div>Home</div>
+        <div>Who am I</div>
+        <div>Projects</div>
+        <div>Contact</div>
+      </div>
+    </div>
+  );
+};
